2b76cc18111cb674c11d8aabd5f75614060c667b
Grzegorz Wasilko <wasilko1@o2.pl>
2021-10-13 15:09:24 +0200

    comment removed , quots repeir

--- main.py
+++ main.py
@@ -5,32 +5,26 @@ import tmdb_client,random
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def homepage():
-#__Użycie funkcji get_movie_info do zbudowania słownika zawierającego_____#
-#__tytuł i URL do plakatu dla każdego filmu, który będziemy wyświetlać____#
-   # ver 1. movies = tmdb_client.get_popular_movies()["results"][:8]
-    selected_list = request.args.get('list_type', "popular")
+
+    selected_list = request.args.get("list_type", "popular")
     genres = tmdb_client.get_genres()
     movies = tmdb_client.get_movies(how_many=8, list_type=selected_list)
     return render_template("homepage.html", movies=movies, current_list=selected_list, genres=genres)
     
-#_________________________________________________________________________#
-###___każdy szablon będzie miał dostępny obiekt o nazwie tmdb_image_url___#
-###___czyli funkcję, która przyjmuje dwa parametry: path oraz size________#
 @app.context_processor
 def utility_processor():
     def tmdb_image_url(path, size):
         return tmdb_client.get_poster_url(path, size)
     return {"tmdb_image_url": tmdb_image_url}
-###_______________________________________________________________________#
 
 @app.route("/movie/<movie_id>")
 def movie_details(movie_id):
     details = tmdb_client.get_single_movie(movie_id)
     movie_images = tmdb_client.get_movie_images(movie_id)
     cast = tmdb_client.get_single_movie_cast(movie_id)
-    selected_backdrop = random.choice(movie_images['backdrops'])
+    selected_backdrop = random.choice(movie_images["backdrops"])
     return render_template("movie_details.html", movie=details, cast=cast, selected_backdrop=selected_backdrop)
 
 
--- tmdb_client.py
+++ tmdb_client.py
@@ -1,24 +1,20 @@
 import requests, random
 API_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGY3NWRhZDNjOWNmMDYwYThiMjY4MGY3NTQxOTE3OSIsInN1YiI6IjYxNWVlZWUzNjdlMGY3MDA0Mzg4YjA0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4TCwbzHc7Wajk13HfNEm4v2AH_DLzQ2x3y2WVpj6Jf4"
 
-###+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-def get_popular_movies():# stworzenie funkcji, która pobierze dane o ____________
-    #____________________#_najpopularniejszych filmach.__________________________
+
+def get_popular_movies():
+  
     endpoint = "https://api.themoviedb.org/3/movie/popular"
     headers = {
         "Authorization": f"Bearer {API_TOKEN}"
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
-###_______________________________________________________________________________
 
-
-#____________________________________Print chwilowo sprawdza zawartość data_______
 data = get_popular_movies()
 for key  in data:
     print( key )
-#_________________________________________________________________________________    
-##_____________Pobiera wybraną listę filmów_______________________________________
+
 def get_movies_list(list_type):
     endpoint = f"https://api.themoviedb.org/3/movie/{list_type}"
     headers = {
@@ -27,18 +23,15 @@ def get_movies_list(list_type):
     response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
     return response.json()
-###_______________________________________________________________________________
-# ______funkcja przyjmuje w parametrze ścieżkę do plakatu oraz rozmiar.___________
-# ______domyślną wartość obrazka ma w342 _________________________________________
+
 def get_poster_url(poster_api_path, size="w342"):
     base_url = "https://image.tmdb.org/t/p/"
     return f"{base_url}{size}/{poster_api_path}"
-###_______________________________________________________________________________
-#__________funkcja przycina listę filmów i tworzy dane do wyśwetlania_____________
+
 def get_movies(how_many, list_type):
     data = get_movies_list(list_type)['results']
     return random.sample(data, k=len(data))[:how_many]
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 def get_genres():
     endpoint = "https://api.themoviedb.org/3/genre/tv/list"
@@ -61,17 +54,13 @@ def get_genre_movies(collection_id):
 
 def list_check(list_type):
     genres = get_genres()
-    genres = [genres['name'] for genres in genres]   
+    genres = [genres["name"] for genres in genres]   
     for genre in genres:
         if list_type == genre:
             return True
         get_movies(how_many=8, list_type="popularr")
     return genres    
 
-
-#=================================================================================
-###_________________________SZABLON MOVIE DETAILS_________________________________
-#__________________def która pobierze szczegóły konkretnego filmu z API. _________
 def get_single_movie(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}"
     headers = {
@@ -79,7 +68,7 @@ def get_single_movie(movie_id):
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
-###_____________________________________________Image____________________________
+
 def get_movie_images(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/images"
     headers = {
@@ -87,7 +76,7 @@ def get_movie_images(movie_id):
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()
-###_____________________________________________Aktors___________________________
+
 def get_single_movie_cast(movie_id):
     endpoint = f"https://api.themoviedb.org/3/movie/{movie_id}/credits"
     headers = {
@@ -95,5 +84,5 @@ def get_single_movie_cast(movie_id):
     }
     response = requests.get(endpoint, headers=headers)
     return response.json()["cast"]
-###===============================================================================
+
 
